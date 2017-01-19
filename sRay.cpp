8a9e77dfd8e3f5721d1ea5b73db75a0f56f19f95
Sruthik P <psruthik@gmail.com>
2017-01-19 18:57:06 +0000

    Working Raytracer Added

--- sRay.cpp
+++ sRay.cpp
@@ -6,25 +6,10 @@
 #include <iostream>
 #include <cassert>
 
-/* Primer for Euclidian Vectors - Not your Data Structure std::vector
-   Both Magnitude and Direction
-   
-   Vector Addition : Add the respective Components
-   Vector Subtraction : Subtract the respective cComponents
-   Vector Scaling : Multiply each component with a scalar
-   Vector Shrink (Scalar Division?) : Divide each component with a scalar
-   Vector Magnitude : Pythagoras Theorem (sqrt(Vx * Vx + Vy * Vy)
-   Vector Normalization : Making the magnitude of the Vector to 1 while keeping the direction constant. Divide the components of the vector with its magnitude
-
-*/
-
-
-//For compiling with VC++ on Windows add #include<algorithm> for std::min and std::max
-
 #if defined __linux__ || defined __APPLE__
-//RTYRTY
+// "Compiled for Linux
 #else
-// VC++ Compiler doesn't define the below symbols by default
+// Windows doesn't define these values by default, Linux does
 #define M_PI 3.141592653589793
 #define INFINITY 1e8
 #endif
@@ -33,15 +18,11 @@ template<typename T>
 class Vec3
 {
 public:
-  T x, y, z;
-  Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-  explicit  Vec3(T xx) : x(xx), y(xx), z(xx) {}// Single parameter Constructor to initialize all 3 components of the Vector with the same value
-  Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}// 3 Parameter Constructor to initialize the 3 components of the Vector uniquely
-  /*Vector Normalize:  
-
-  */
-  
-  Vec3& normalize() // Returns normalized vector
+    T x, y, z;
+    Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+    Vec3(T xx) : x(xx), y(xx), z(xx) {}
+    Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+    Vec3& normalize()
     {
         T nor2 = length2();
         if (nor2 > 0) {
@@ -50,16 +31,16 @@ public:
         }
         return *this;
     }
-  Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); } // Scalar Multiplication
-  Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }//Vector Multiplication
-  T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }//Dot Product, Returns a Single Value
-  Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }// Vector Subtraction
-  Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }// Vector Addition
+    Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
+    Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
+    T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
+    Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
+    Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
     Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
     Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
-  Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }//Inverts Direction
-  T length2() const { return x * x + y * y + z * z; }// Returns product of all components
-  T length() const { return sqrt(length2()); } // Returns magnitude of the Vector
+    Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
+    T length2() const { return x * x + y * y + z * z; }
+    T length() const { return sqrt(length2()); }
     friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
     {
         os << "[" << v.x << " " << v.y << " " << v.z << "]";
@@ -72,31 +53,27 @@ typedef Vec3<float> Vec3f;
 class Sphere
 {
 public:
-    Vec3f center;                           // position of the sphere
-    float radius, radius2;                  // sphere radius and radius^2
-    Vec3f surfaceColor, emissionColor;      // surface color and emission (light)
-    float transparency, reflection;         // surface transparency and reflectivity
+    Vec3f center;                           /// position of the sphere
+    float radius, radius2;                  /// sphere radius and radius^2
+    Vec3f surfaceColor, emissionColor;      /// surface color and emission (light)
+    float transparency, reflection;         /// surface transparency and reflectivity
     Sphere(
         const Vec3f &c,
         const float &r,
         const Vec3f &sc,
         const float &refl = 0,
         const float &transp = 0,
-        const Vec3f &ec = 0) :center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec), transparency(transp), reflection(refl)
+        const Vec3f &ec = 0) :
+        center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec),
+        transparency(transp), reflection(refl)
     { /* empty */ }
-  
-    //Default Constructor for Sphere
- 
 
-  /* Primer about Ray Sphere Intersection. Refer to Peter Shirley's Book, pg 76.
-     The Ray and Sphere equation must both be equal for the ray to intersect with the sphere. Equate both to figure out if the Ray intersects the sphere
-  */
     bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
     {
-      Vec3f l = center - rayorig; // Distance between the centre of the sphere and the origin of the ray
-      float tca = l.dot(raydir); // Figuring out the angle between the Dir Vector and the Vector between the Origin and centre of the sphere
-      if (tca < 0) return false; // If the angle between them is 90 i.e orthogonal ray doesn't hit the sphere.
-      float d2 = l.dot(l) - tca * tca; //
+        Vec3f l = center - rayorig;
+        float tca = l.dot(raydir);
+        if (tca < 0) return false;
+        float d2 = l.dot(l) - tca * tca;
         if (d2 > radius2) return false;
         float thc = sqrt(radius2 - d2);
         t0 = tca - thc;
@@ -106,9 +83,6 @@ public:
     }
 };
 
-//[comment]
-// This variable controls the maximum recursion depth
-//[/comment]
 #define MAX_RAY_DEPTH 5
 
 float mix(const float &a, const float &b, const float &mix)
@@ -116,16 +90,6 @@ float mix(const float &a, const float &b, const float &mix)
     return b * mix + a * (1 - mix);
 }
 
-//[comment]
-// This is the main trace function. It takes a ray as argument (defined by its origin
-// and direction). We test if this ray intersects any of the geometry in the scene.
-// If the ray intersects an object, we compute the intersection point, the normal
-// at the intersection point, and shade this point using this information.
-// Shading depends on the surface property (is it transparent, reflective, diffuse).
-// The function returns a color for the ray. If the ray intersects an object that
-// is the color of the object at the intersection point, otherwise it returns
-// the background color.
-//[/comment]
 Vec3f trace(
     const Vec3f &rayorig,
     const Vec3f &raydir,
@@ -147,15 +111,11 @@ Vec3f trace(
         }
     }
     // if there's no intersection return black or background color
-    if (!sphere) return Vec3f(1);
+    if (!sphere) return Vec3f(2);
     Vec3f surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
     Vec3f phit = rayorig + raydir * tnear; // point of intersection
     Vec3f nhit = phit - sphere->center; // normal at the intersection point
     nhit.normalize(); // normalize normal direction
-    // If the normal and the view direction are not opposite to each other
-    // reverse the normal direction. That also means we are inside the sphere so set
-    // the inside bool to true. Finally reverse the sign of IdotN which we want
-    // positive.
     float bias = 1e-4; // add some bias to the point from which we will be tracing
     bool inside = false;
     if (raydir.dot(nhit) > 0) nhit = -nhit, inside = true;
@@ -209,14 +169,10 @@ Vec3f trace(
     return surfaceColor + sphere->emissionColor;
 }
 
-//[comment]
-// Main rendering function. We compute a camera ray for each pixel of the image
-// trace it and return a color. If the ray hits a sphere, we return the color of the
-// sphere at the intersection point, else we return the background color.
-//[/comment]
+
 void render(const std::vector<Sphere> &spheres)
 {
-    unsigned width = 640, height = 480;
+    unsigned width = 1920, height = 1080;
     Vec3f *image = new Vec3f[width * height], *pixel = image;
     float invWidth = 1 / float(width), invHeight = 1 / float(height);
     float fov = 30, aspectratio = width / float(height);
@@ -243,21 +199,18 @@ void render(const std::vector<Sphere> &spheres)
     delete [] image;
 }
 
-//[comment]
-// In the main function, we will create the scene which is composed of 5 spheres
-// and 1 light (which is also a sphere). Then, once the scene description is complete
-// we render that scene, by calling the render() function.
-//[/comment]
+
 int main(int argc, char **argv)
 {
     srand48(13);
     std::vector<Sphere> spheres;
     // position, radius, surface color, reflectivity, transparency, emission color
-    spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-    spheres.push_back(Sphere(Vec3f( 0.0,      0, -20),     4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
-    spheres.push_back(Sphere(Vec3f( 5.0,     -1, -15),     2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-    spheres.push_back(Sphere(Vec3f( 5.0,      0, -25),     3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-    spheres.push_back(Sphere(Vec3f(-5.5,      0, -15),     3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
+    spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 2,Vec3f(1.00, 0.32, 0.36), 0, 0.0));
+    spheres.push_back(Sphere(Vec3f( 0.0,      0, -25),     2, Vec3f(1.00, 0.32, 0.36), 0, 0.5));//red
+    spheres.push_back(Sphere(Vec3f( 8.0,     -4, -25),     2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));//yellow
+    spheres.push_back(Sphere(Vec3f( 8.0,      4, -25),     2, Vec3f(0.65, 0.77, 0.97), 1, 0.0));//blue
+    spheres.push_back(Sphere(Vec3f(-8.0,      4, -25),     2, Vec3f(0.90, 0.90, 0.90), 1, 0.0));//black
+    spheres.push_back(Sphere(Vec3f(-8.0,     -4, -25), 2 ,Vec3f(0.00, 1.00, 0.00), 1, 0.0));//green
     // light
     spheres.push_back(Sphere(Vec3f( 0.0,     20, -30),     3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
     render(spheres);
